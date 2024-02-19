0f9c51944d3f3234fefc4bd19709ec77abb05fe3
WENDAOZ-Van <z2219091891@gmail.com>
2024-02-19 14:08:40 +0000

    try to add some visual things in canvas

--- script.js
+++ script.js
@@ -1,63 +1,36 @@
 document.getElementById('getPostalCode').addEventListener('click', function() {
-  if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-          const lat = position.coords.latitude;
-          const lng = position.coords.longitude;
-
-          fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyBbrGOproJ4hTb-rsKwiLXo5MaDFRCoTPU`)
-              .then(response => response.json())
-              .then(data => {
-                  const postalCode = data.results[0].address_components.find(component => component.types.includes('postal_code')).long_name;
-                  setPostalCode(postalCode);
-              })
-              .catch(error => console.error('Error:', error));
-      }, function() {
-          alert('Geolocation is not supported by this browser.');
-      });
-  } else {
-      alert('Geolocation is not supported by this browser.');
-  }
+    // 模拟获取邮政编码
+    const postalCode = '96382'; // 假设的邮政编码
+    setPostalCode(postalCode);
 });
 
 function setPostalCode(postalCode) {
-  document.getElementById('postalCode').textContent = postalCode;
-  playPostalCodeSound(postalCode);
+    document.getElementById('postalCode').textContent = postalCode;
+    playPostalCodeSound(postalCode);
 }
 
 function playPostalCodeSound(postalCode) {
-  const synth = new Tone.Synth().toDestination();
-  const now = Tone.now();
+    const synth = new Tone.Synth().toDestination();
+    const now = Tone.now();
+    let delayTime = 0;
 
-const osc = new Tone.Oscillator().toDestination();
-// start at "C4"
-osc.frequency.value = "C4";
-// ramp to "C2" over 2 seconds
-osc.frequency.rampTo("C2", 2);
-// start the oscillator for 2 seconds
-osc.start().stop("+1");
-
-  let delayTime = 0;
-
-  postalCode.split('').forEach((char, index) => {
-      let note = '';
-      switch(char.toUpperCase()) {
-          // 映射关系
-          case '0': case 'A': note = 'C4'; break;
-          case '1': case 'B': note = 'D4'; break;
-          case '2': case 'C': note = 'E4'; break;
-          case '3': case 'D': note = 'F4'; break;
-          case '4': case 'E': note = 'G4'; break;
-          case '5': case 'F': note = 'A4'; break; 
-          case '6': case 'G': note = 'B4'; break;
-          case '7': case 'H': note = 'C5'; break;
-          case '8': case 'I': note = 'D5'; break;
-          case '9': case 'J': note = 'E5'; break;
-          default: note = 'C4';
-      }
-      synth.triggerAttackRelease(note, "8n", now + delayTime);
-      delayTime += 0.5;
-    
-    
-  });
+    postalCode.split('').forEach((char, index) => {
+        let note = '';
+        switch(char.toUpperCase()) {
+            case '0': case 'A': note = 'C4'; break;
+            case '1': case 'B': note = 'D4'; break;
+            case '2': case 'C': note = 'E4'; break;
+            case '3': case 'D': note = 'F4'; break;
+            case '4': case 'E': note = 'G4'; break;
+            case '5': case 'F': note = 'A4'; break; 
+            case '6': case 'G': note = 'B4'; break;
+            case '7': case 'H': note = 'C5'; break;
+            case '8': case 'I': note = 'D5'; break;
+            case '9': case 'J': note = 'E5'; break;
+            // 添加更多映射关系...
+            default: note = 'C4';
+        }
+        synth.triggerAttackRelease(note, "8n", now + delayTime);
+        delayTime += 0.5;
+    });
 }
-
